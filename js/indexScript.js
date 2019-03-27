3243edb670f490f1aab959b6234d52a988485655
mrthomasj <tr.jehlen@outlook.com>
2019-03-27 16:15:45 -0300

    Sidenav Done
    Sidenav structure done, fixed activation sidebar.

--- js/indexScript.js
+++ js/indexScript.js
@@ -1,10 +1,12 @@
 function openNav(){
     document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("main").style.marginLeft = "170px";
+    document.getElementsById("main").style.padding = "20px";
 }
 
 function closeNav(){
     
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
+    document.getElementsById("main").style.padding = "20px 20px 20px 102px";
 }
