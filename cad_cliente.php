b5bdd87cd7666cf08fd15b30425e48446e2784dd
mrthomasj <tr.jehlen@outlook.com>
2019-03-28 14:44:08 -0300

    Start of Client Form

--- cad_cliente.php
+++ cad_cliente.php
@@ -12,7 +12,7 @@
                 <div onclick="openNav()" class="openMenu">MENU</div>
                 <div id="mySidenav" class="sidenav">
                     <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-                    <a href="#">Cadastro de cliente</a>
+                    <a href="cad_cliente.php">Cadastro de cliente</a>
                     <a href="#">Cadastro de veículo</a>
                     <a href="#">Consulta de locação</a>
                     <a href="#">Agendamento de locação</a>
@@ -26,7 +26,19 @@
                     </div>
                     
                     <div class="form-container">
-                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nisl nunc, lacinia at maximus ac, bibendum ut nisl. Integer volutpat ante nibh, non ullamcorper dui bibendum ac. Sed congue ornare massa, ut accumsan mauris condimentum eu. Morbi tincidunt, orci id molestie luctus, tortor velit aliquet est, ut dictum mauris ipsum quis nulla. Sed pharetra id massa non cursus. Cras dui purus, eleifend in tincidunt vel, tempor sit amet nisl. In maximus enim a enim dapibus, vel dictum ex iaculis. Cras risus purus, semper ac volutpat euismod, elementum varius felis. Mauris eget dolor nisl. Nullam quis augue eget augue vulputate iaculis vel et lorem. Duis aliquet diam gravida, maximus ex non, placerat turpis. Duis efficitur efficitur luctus. Sed ipsum nisi, scelerisque pellentesque massa et, sodales maximus dolor. Sed sed nisi hendrerit, vehicula odio vel, eleifend eros. Donec hendrerit eleifend est, vel pulvinar enim commodo consequat. Phasellus finibus egestas purus, eu vestibulum eros semper sed.
+                        <form name="cadCli" method="post" action="#">
+                            <div class="form-group">
+                            <label>CPF:</label>
+                            <input type="text" name="cpf" id="cpf">
+                            </div>
+                            <div class="form-group">
+                            <label>Nome:</label>
+                            <input type="text" name="cliNome" id="cliNm">
+                            </div>
+                            <label>Data de Nascimento:</label>
+                            <input type="date" name="dtNasc" id="dtNasc"><br>
+                            
+                        </form>
                     </div>
                     
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nisl nunc, lacinia at maximus ac, bibendum ut nisl. Integer volutpat ante nibh, non ullamcorper dui bibendum ac. Sed congue ornare massa, ut accumsan mauris condimentum eu. Morbi tincidunt, orci id molestie luctus, tortor velit aliquet est, ut dictum mauris ipsum quis nulla. Sed pharetra id massa non cursus. Cras dui purus, eleifend in tincidunt vel, tempor sit amet nisl. In maximus enim a enim dapibus, vel dictum ex iaculis. Cras risus purus, semper ac volutpat euismod, elementum varius felis. Mauris eget dolor nisl. Nullam quis augue eget augue vulputate iaculis vel et lorem. Duis aliquet diam gravida, maximus ex non, placerat turpis. Duis efficitur efficitur luctus. Sed ipsum nisi, scelerisque pellentesque massa et, sodales maximus dolor. Sed sed nisi hendrerit, vehicula odio vel, eleifend eros. Donec hendrerit eleifend est, vel pulvinar enim commodo consequat. Phasellus finibus egestas purus, eu vestibulum eros semper sed.
